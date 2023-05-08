2571877ca60714d089ede9a83216795fc4bb1b28
Indi Esneault <indiesneault@gmail.com>
2023-05-08 16:00:25 -0700

    reformat

--- pushPopPeek/__main__.py
+++ pushPopPeek/__main__.py
@@ -17,39 +17,56 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 TAN = (255, 220, 110)
 
-BLACK_STONE = pygame.transform.scale(pygame.image.load("images/BlackGoStone.png"), (WIDTH/5,WIDTH/5))
-WHITE_STONE = pygame.transform.scale(pygame.image.load("images/WhiteGoStone.png"), (WIDTH/5,WIDTH/5))
-RED_STONE = pygame.transform.scale(pygame.image.load("images/RedGoStone.png"), (WIDTH/5,WIDTH/5))
-BlackButton = pygame.transform.scale(pygame.image.load("images/BlackButton.png"), (GUI_WIDTH*.8,GUI_WIDTH*.2))
-WhiteButton = pygame.transform.scale(pygame.image.load("images/WhiteButton.png"), (GUI_WIDTH*.8,GUI_WIDTH*.2))
-Neutral = pygame.transform.scale(pygame.image.load("images/RedButton.png"), (GUI_WIDTH*.8,GUI_WIDTH*.2))
-Remove = pygame.transform.scale(pygame.image.load("images/Remove.png"), (GUI_WIDTH*.8,GUI_WIDTH*.2))
-EndTurn = pygame.transform.scale(pygame.image.load("images/EndTurn.png"), (GUI_WIDTH*.8,GUI_WIDTH*.2))
-BACKGROUND = pygame.transform.scale(pygame.image.load("images/wood.jpg"), (WIDTH,WIDTH))
-BLACKWIN = pygame.transform.scale(pygame.image.load("images/BlackWins.png"), (WIDTH*.75,WIDTH*.75))
-WHITEWIN = pygame.transform.scale(pygame.image.load("images/WhiteWins.png"), (WIDTH*.75,WIDTH*.75))
-OUTOFMOVES = pygame.transform.scale(pygame.image.load("images/OutOfMoves.png"), (WIDTH*.75,WIDTH*.75))
+BLACK_STONE = pygame.transform.scale(pygame.image.load(
+    "images/BlackGoStone.png"), (WIDTH/5, WIDTH/5))
+WHITE_STONE = pygame.transform.scale(pygame.image.load(
+    "images/WhiteGoStone.png"), (WIDTH/5, WIDTH/5))
+RED_STONE = pygame.transform.scale(pygame.image.load(
+    "images/RedGoStone.png"), (WIDTH/5, WIDTH/5))
+BlackButton = pygame.transform.scale(pygame.image.load(
+    "images/BlackButton.png"), (GUI_WIDTH*.8, GUI_WIDTH*.2))
+WhiteButton = pygame.transform.scale(pygame.image.load(
+    "images/WhiteButton.png"), (GUI_WIDTH*.8, GUI_WIDTH*.2))
+Neutral = pygame.transform.scale(pygame.image.load(
+    "images/RedButton.png"), (GUI_WIDTH*.8, GUI_WIDTH*.2))
+Remove = pygame.transform.scale(pygame.image.load(
+    "images/Remove.png"), (GUI_WIDTH*.8, GUI_WIDTH*.2))
+EndTurn = pygame.transform.scale(pygame.image.load(
+    "images/EndTurn.png"), (GUI_WIDTH*.8, GUI_WIDTH*.2))
+BACKGROUND = pygame.transform.scale(
+    pygame.image.load("images/wood.jpg"), (WIDTH, WIDTH))
+BLACKWIN = pygame.transform.scale(pygame.image.load(
+    "images/BlackWins.png"), (WIDTH*.75, WIDTH*.75))
+WHITEWIN = pygame.transform.scale(pygame.image.load(
+    "images/WhiteWins.png"), (WIDTH*.75, WIDTH*.75))
+OUTOFMOVES = pygame.transform.scale(pygame.image.load(
+    "images/OutOfMoves.png"), (WIDTH*.75, WIDTH*.75))
 
 dis_to_cen = WIDTH // ROWS // 2
 
 center_x = GUI_WIDTH + WIDTH/2
 center_y = WIDTH/2
 
+
 def init_buttons():
     global images, buttons, BlackButtonY, NeutralY, RemoveY, UndoY, EndTurnY
-    images[GUI_WIDTH/2,WIDTH*.4] = BlackButton
-    images[GUI_WIDTH/2,WIDTH*.525] = Neutral
-    images[GUI_WIDTH/2,WIDTH*.65] = Remove
-    images[GUI_WIDTH/2,WIDTH*.9] = EndTurn
+    images[GUI_WIDTH/2, WIDTH*.4] = BlackButton
+    images[GUI_WIDTH/2, WIDTH*.525] = Neutral
+    images[GUI_WIDTH/2, WIDTH*.65] = Remove
+    images[GUI_WIDTH/2, WIDTH*.9] = EndTurn
     BlackButtonY = int(WIDTH*.4-WIDTH*.05)
     NeutralY = int(WIDTH*.525-WIDTH*.05)
     RemoveY = int(WIDTH*.65-WIDTH*.05)
     EndTurnY = int(WIDTH*.9-WIDTH*.05)
-    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,BlackButtonY,GUI_WIDTH*.8,GUI_WIDTH*.2)) # both
-    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,NeutralY,GUI_WIDTH*.8,GUI_WIDTH*.2)) # 3 red
-    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,RemoveY,GUI_WIDTH*.8,GUI_WIDTH*.2)) # remove
-    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,EndTurnY,GUI_WIDTH*.8,GUI_WIDTH*.2)) # end turn
-    
+    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,
+                   BlackButtonY, GUI_WIDTH*.8, GUI_WIDTH*.2))  # both
+    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,
+                   NeutralY, GUI_WIDTH*.8, GUI_WIDTH*.2))  # 3 red
+    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,
+                   RemoveY, GUI_WIDTH*.8, GUI_WIDTH*.2))  # remove
+    buttons.append(pygame.Rect(GUI_WIDTH/2-GUI_WIDTH*.4,
+                   EndTurnY, GUI_WIDTH*.8, GUI_WIDTH*.2))  # end turn
+
 
 def init_grid():
     dis_to_cen = WIDTH // ROWS // 2
@@ -58,7 +75,8 @@ def init_grid():
     coord_array = [[None for i in range(4)] for j in range(4)]
     stack_array = [[Stack() for i in range(4)] for j in range(4)]
 
-    game_array = [[None, None, None, None], [None, None, None, None], [None, None, None, None], [None, None, None, None]]
+    game_array = [[None, None, None, None], [None, None, None, None], [
+        None, None, None, None], [None, None, None, None]]
 
     for i in range(len(game_array)):
         for j in range(len(game_array[i])):
@@ -66,9 +84,10 @@ def init_grid():
             y = dis_to_cen * (2 * i + 1)
 
             # Adding center coordinates
-            coord_array[i][j] = (x,y)
+            coord_array[i][j] = (x, y)
 
-    return (stack_array,coord_array)
+    return (stack_array, coord_array)
+
 
 def draw_grid():
     gap = WIDTH // ROWS
@@ -84,6 +103,7 @@ def draw_grid():
         pygame.draw.line(win, GRAY, (x, 0), (x, WIDTH), 3)
         pygame.draw.line(win, GRAY, (GUI_WIDTH, y), (WIDTH + GUI_WIDTH, y), 3)
 
+
 def click(stack_array, coord_array):
     global x_turn, images, max_moves, mode, buttons_locked, out_of_moves
     # check if any buttons are clicked
@@ -92,25 +112,25 @@ def click(stack_array, coord_array):
     for i in clicked_sprites:
         print(i)
         if not buttons_locked:
-            if i.y == BlackButtonY: # place 1 of each
+            if i.y == BlackButtonY:  # place 1 of each
                 mode = "both"
                 max_moves = 2
                 buttons_locked = True
                 print("both")
 
-            elif i.y == NeutralY: # place 3 neutral
+            elif i.y == NeutralY:  # place 3 neutral
                 mode = "neutral"
                 max_moves = 3
                 buttons_locked = True
                 print("neutral")
 
-            elif i.y == RemoveY: # remove 2
+            elif i.y == RemoveY:  # remove 2
                 mode = "remove"
                 max_moves = 2
                 buttons_locked = True
                 print("remove")
 
-        if i.y == EndTurnY: # end turn
+        if i.y == EndTurnY:  # end turn
             buttons_locked = False
             print("end turn")
             mode = "both"
@@ -137,12 +157,12 @@ def click(stack_array, coord_array):
 
                 if dis < WIDTH // ROWS // 2:
                     # remove previous piece
-                    
+
                     if piece:
                         # print(piece, 'peek')
                         # print(stack.print())
                         try:
-                            images.pop((x, y)) # add to stack
+                            images.pop((x, y))  # add to stack
                         except:
                             print('error')
                     max_moves -= 1
@@ -166,14 +186,14 @@ def click(stack_array, coord_array):
                         #     images.pop((x,y))
                         if not stack.pop():
                             max_moves += 1
-                        
+
                         if stack.peek() == 'white':
-                            images[(x,y)] = WHITE_STONE
+                            images[(x, y)] = WHITE_STONE
                         elif stack.peek() == 'black':
-                            images[(x,y)] = BLACK_STONE
+                            images[(x, y)] = BLACK_STONE
                         elif stack.peek() == 'red':
-                            images[(x,y)] = RED_STONE
-                        
+                            images[(x, y)] = RED_STONE
+
 
 def win_game(color):
     global run, winImage
@@ -184,7 +204,7 @@ def win_game(color):
         winImage = WHITEWIN
         # images[center_x, center_y] = WHITEWIN
     run = False
-                    
+
 
 def check_win(stack_array):
     # check rows
@@ -203,7 +223,7 @@ def check_win(stack_array):
 
     # # check diagonals
     diag1 = set()
-    
+
     diag2 = set()
 
     for i in range(len(stack_array)):
@@ -215,16 +235,20 @@ def check_win(stack_array):
     if len(diag2) == 1 and None not in diag2 and 'red' not in diag2:
         win_game(stack_array[len(stack_array) - 1][0].peek())
 
+
 def render():
     win.fill(TAN)
     draw_grid()
-    for x,y in images:
-        IMAGE = images[(x,y)]
-        win.blit(IMAGE, (x - IMAGE.get_width() // 2, y - IMAGE.get_height() // 2))
+    for x, y in images:
+        IMAGE = images[(x, y)]
+        win.blit(IMAGE, (x - IMAGE.get_width() //
+                 2, y - IMAGE.get_height() // 2))
 
     if out_of_moves:
-        win.blit(OUTOFMOVES, (center_x - OUTOFMOVES.get_width() // 2, center_y - OUTOFMOVES.get_height() // 2))
+        win.blit(OUTOFMOVES, (center_x - OUTOFMOVES.get_width() //
+                 2, center_y - OUTOFMOVES.get_height() // 2))
     pygame.display.update()
+
 
 def main():
     global x_turn, images, buttons, mode, buttons_locked, max_moves, run, winImage, out_of_moves
@@ -235,13 +259,12 @@ def main():
     x_turn = True
     stack_array, coord_array = init_grid()
     images[(GUI_WIDTH+WIDTH/2, WIDTH/2)] = BACKGROUND
-    
 
     mode = "both"
     max_moves = 2
     out_of_moves = False
     buttons_locked = False
-    
+
     init_buttons()
 
     while run:
@@ -257,16 +280,10 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pygame.quit()
-        win.blit(winImage, (center_x - winImage.get_width() // 2, center_y - winImage.get_height() // 2))
+        win.blit(winImage, (center_x - winImage.get_width() //
+                 2, center_y - winImage.get_height() // 2))
         pygame.display.update()
 
 
 if __name__ == '__main__':
     main()
-
-# 4x4
-
-# moves:
-# place up to 3 neutral
-# place 1 colored + 1 neutral
-# remove 2 pieces
